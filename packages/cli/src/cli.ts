250b1aef2d6e5ef4bbec04a463cb7ab8fa433387
Felix <felix.mattick@gmail.com>
2020-03-29 02:33:22 -0500

    I'm so stupid

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -1,6 +1,6 @@
 const [_, ...args] = process.argv
 
-if (args[0] === 'server') {
+if (args[1] === 'server') {
   import('./server')
 } else {
   import('./client').then(({ go }) => go(args[1] ?? 'ws://localhost:8080'))
